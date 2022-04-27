aafb1eba13f7f3ca7a292801424de0832d796d8e
Ilya <yowazza1@gmail.com>
2022-04-27 21:10:09 +0300

    store a table is created

--- resources/views/admin/tables/create.blade.php
+++ resources/views/admin/tables/create.blade.php
@@ -13,25 +13,44 @@
             </div>
             <div class="m-2 p-2 bg-slate-100 rounded">
             <div class="space-y-8 divide-y divide-gray-200 w-1/2 mt-10">
-                <form enctype="multipart/form-data">
-                  <div class="sm:col-span-6">
-                    <label for="title" class="block text-sm font-medium text-gray-700"> Name </label>
-                    <div class="mt-1">
-                      <input type="text" id="title" wire:model.lazy="title" name="title" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                <form method="POST" action="{{ route('admin.tables.store') }}">
+                    @csrf
+                    <div class="sm:col-span-6">
+                        <label for="name" class="block text-sm font-medium text-gray-700"> Name </label>
+                        <div class="mt-1">
+                            <input type="text" id="name" name="name"
+                                class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        </div>
                     </div>
-                  </div>
-                  <div class="sm:col-span-6">
-                    <label for="title" class="block text-sm font-medium text-gray-700"> Image </label>
-                    <div class="mt-1">
-                      <input type="file" id="image" wire:model="newImage" name="image" class="block w-full transition duration-150 ease-in-out appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                    <div class="sm:col-span-6">
+                        <label for="guest_number" class="block text-sm font-medium text-gray-700"> Guest Number
+                        </label>
+                        <div class="mt-1">
+                            <input type="number" id="guest_number" name="guest_number"
+                                class="block w-full appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out sm:text-sm sm:leading-5" />
+                        </div>
                     </div>
-                  </div>
-                  <div class="sm:col-span-6 pt-5">
-                    <label for="body" class="block text-sm font-medium text-gray-700">Description</label>
-                    <div class="mt-1">
-                      <textarea id="body" rows="3" wire:model.lazy="body" class="shadow-sm focus:ring-indigo-500 appearance-none bg-white border border-gray-400 rounded-md py-2 px-3 text-base leading-normal transition duration-150 ease-in-out focus:border-indigo-500 block w-full sm:text-sm border-gray-300 rounded-md"></textarea>
+                    <div class="sm:col-span-6 pt-5">
+                        <label for="status" class="block text-sm font-medium text-gray-700">Status</label>
+                        <div class="mt-1">
+                            <select id="status" name="status" class="form-multiselect block w-full mt-1">
+                                @foreach (App\Enums\TableStatus::cases() as $status)
+                                    <option value="{{ $status->value }}">{{ $status->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
                     </div>
-                  </div>
+                    <div class="sm:col-span-6 pt-5">
+                        <label for="location" class="block text-sm font-medium text-gray-700">Location</label>
+                        <div class="mt-1">
+                            <select id="location" name="location" class="form-multiselect block w-full mt-1">
+                                @foreach (App\Enums\TableLocation::cases() as $location)
+                                    <option value="{{ $location->value }}">{{ $location->name }}</option>
+                                @endforeach
+                            </select>
+                        </div>
+                    </div>
+
                   <div class="mt-6 p-4">
                         <button type="submit" class="px-4 py-2 bg-indigo-500 hover:bg-indigo-700 rounded-lg text-white">Store</button>
                   </div>
