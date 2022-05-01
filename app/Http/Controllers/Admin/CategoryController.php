1a9769434684411e4c8a5a8b808c2eb444df0579
Ilya <yowazza1@gmail.com>
2022-05-01 19:13:53 +0300

    fixed delete category error

--- app/Http/Controllers/Admin/CategoryController.php
+++ app/Http/Controllers/Admin/CategoryController.php
@@ -110,6 +110,7 @@ class CategoryController extends Controller
     public function destroy(Category $category)
     {
         Storage::delete($category->image);
+        $category->menus()->detach();
         $category->delete();
         return to_route('admin.categories.index')->with('danger', 'Category deleted successfully');
     }
