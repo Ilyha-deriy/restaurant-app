7959c64015dd8b65ceb05a5c5c9bd9222dc28e95
Ilya <yowazza1@gmail.com>
2022-05-03 18:00:48 +0300

    created routes for additional controllers

--- routes/web.php
+++ routes/web.php
@@ -5,6 +5,9 @@ use App\Http\Controllers\Admin\CategoryController;
 use App\Http\Controllers\Admin\MenuController;
 use App\Http\Controllers\Admin\ReservationController;
 use App\Http\Controllers\Admin\TableController;
+use App\Http\Controllers\Frontend\CategoryController as FrontendCategoryController;
+use App\Http\Controllers\Frontend\MenuController as FrontendMenuController;
+use App\Http\Controllers\Frontend\ReservationController as FrontendReservationController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Middleware\Admin;
 use App\Models\Category;
@@ -23,6 +26,15 @@ use App\Models\Category;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/categories', [FrontendCategoryController::class, 'index'])->name('categories.index');
+Route::get('/categories/{category}', [FrontendCategoryController::class, 'show'])->name('categories.show');
+Route::get('/menus', [FrontendMenuController::class, 'index'])->name('menus.index');
+Route::get('/reservation/step-one', [FrontendReservationController::class, 'stepOne'])->name('reservations.step.one');
+Route::post('/reservation/step-one', [FrontendReservationController::class, 'storeStepOne'])->name('reservations.store.step.one');
+Route::get('/reservation/step-two', [FrontendReservationController::class, 'stepTwo'])->name('reservations.step.two');
+Route::post('/reservation/step-two', [FrontendReservationController::class, 'storeStepTwo'])->name('reservations.store.step.two');
+
 
 Route::get('/dashboard', function () {
     return view('dashboard');
